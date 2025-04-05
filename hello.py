7b2d511a21895f7c590e7f4ff2b6aef9e800742d
Lawrence Adu-Gyamfi <Lawrence-asamoah.adu-gyamfi@totalenergies.com>
2025-04-05 16:47:19 +0200

    feat: add first draft with TDD

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ml-microbiome-package!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+rf_model = None
+
+data = np.random.randint(0, 100, 100)
+
+def predict(model, data, type):
+    return data
+
+
