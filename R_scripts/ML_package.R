9a52fe31b77736efe59e39e2097efac09042166b
Lawrence Adu-Gyamfi <Lawrence-asamoah.adu-gyamfi@totalenergies.com>
2025-04-18 01:27:24 +0200

    add working version

--- R_scripts/ML_package.R
+++ R_scripts/ML_package.R
@@ -1,3 +1,4 @@
+#%%
 library(caret)
 library(randomForest)
 library(compositions) 
@@ -8,19 +9,19 @@ library(tibble)
 #1.Data filtering
 
 #Read abundance file
-abundance <- read_tsv("/Users/cimi_bioinformatics/Desktop/abundance_crc.txt")
-otu_table<- column_to_rownames(abundance, var = "Species")
+abundance <- read_tsv("/Users/lawrenceadu-gyamfi/Documents/PERSONAL/PROJECTS/ML_Microbiome_Package/Dataset/abundance_crc.txt")
+otu_table <- column_to_rownames(abundance, var = "Species")
 
 #Read metadata file
-metadata <- read_tsv("/Users/cimi_bioinformatics/Desktop/metadata_crc.txt")
+metadata <- read_tsv("/Users/lawrenceadu-gyamfi/Documents/PERSONAL/PROJECTS/ML_Microbiome_Package/Dataset/metadata_crc.txt")
 sample.labels <- as.factor(na.omit(metadata$Group))
 
-
+#%%
 #Example of thresholds for abundance and prevalence
 abundance_cutoff <- 0.000001
 prevalence_cutoff <- 0.05
 
-otu_table <- abundance
+# otu_table <- abundance
 
 mean_abundance <- rowMeans(otu_table)
 
@@ -34,7 +35,7 @@ prevalence <- rowMeans(otu_table_abundance_filtered > 0)
 #Data filtering using thresholds to obtain final dataframe for downstream data normalization
 taxa_prevalence_filtered <- names(prevalence[prevalence >= prevalence_cutoff])
 otu_table_final <- otu_table_abundance_filtered[taxa_prevalence_filtered, , drop = FALSE]
-
+#%%
 
 #2.Data normalization
 #centered-log_ratio_transformation
@@ -51,18 +52,20 @@ otu_table_clr <- clr_transform(otu_table_final)
 
 #Setting cross_validation parameters
 set.seed(42)
-train_control <- trainControl(method = "repeatedcv", number = 10, repeats = 20)
+train_control <- trainControl(method = "repeatedcv", number = 5, repeats = 2)
 
 #Training the model using Random Forest
-x <- as.matrix(otu_table_clr) 
+# x <- as.matrix(otu_table_clr) 
+x <- t(as.matrix(otu_table_clr))
 y <- sample.labels             #Needs to be a factors (metadata) that correspond to the samples
-
-rf_model <- train(x, y, 
+#%%
+rf_model <- train(x, y,
                   method = "rf",
                   trControl = train_control,
-                  tuneLength = 5)  
+                  tuneLength = 5,
+                  ntree=100)
 
-
+#%%
 
 
 pred_probs <- predict(rf_model, x, type = "prob")
@@ -71,4 +74,3 @@ pred_probs <- predict(rf_model, x, type = "prob")
 #4.Model evaluation
 
 #Plotting ROC curves
-
