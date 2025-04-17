3e19d9114b7e3ec34e4b12483c7edaab9154314b
Lawrence Adu-Gyamfi <Lawrence-asamoah.adu-gyamfi@totalenergies.com>
2025-04-17 17:08:31 +0200

    first version of python

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ rf_model = None
 
 data = np.random.randint(0, 100, 100)
 
+abundance_cutoff = 0.00001
+prevalence_cutoff = 0.05
+
+mean_abundance = None  # TODO
+
+def clr_transform(data, log_n0: float = 1e-06):
+    pass
+
 def predict(model, data, type):
     return data
 
