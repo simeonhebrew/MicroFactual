35cb1e2343cd7072a57955b7bf68872aa1885924
Lawrence <bh-204@hotmail.com>
2025-06-25 00:04:57 +0200

    feat: make modelling more generic

--- src/microbiome_ml/modeling.py
+++ src/microbiome_ml/modeling.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RepeatedKFold, GridSearchCV
 import numpy as np
 
-def train_model(X, y, cv_splits=2, n_jobs=None):
+def train_model(X, y, cv_splits=2, n_jobs=None, n_estimators=100):
     """Train Random Forest classifier with cross-validation
     
     Parameters:
@@ -18,7 +18,7 @@ def train_model(X, y, cv_splits=2, n_jobs=None):
         Set to 1 to avoid multiprocessing issues.
     """
     param_grid = {'max_features': np.unique(np.linspace(1, X.shape[1], 5, dtype=int))}
-    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
     cv = RepeatedKFold(n_splits=cv_splits, n_repeats=2, random_state=42)
 
     grid_search = GridSearchCV(rf, param_grid, cv=cv, n_jobs=n_jobs)
